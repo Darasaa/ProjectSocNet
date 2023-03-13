069766b0b63fac269f21360d9deb0010a54bc422
Darasa <darasa.daraselia@gmail.com>
2023-03-13 18:09:16 +0400

    Add databse manager + login error jsp

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -1,2 +1,59 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class DatabaseManager {
+    private static final String DATABASE_NAME = "baza";
+    private static final String URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "shechema";
+    private static final String TABLE_NAME = "accounts";
+
+    public void insert(User user) {
+        try {
+            Class.forName(DRIVER);
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            String query = "insert into " + TABLE_NAME +"(FirstName, LastName, Age) values (?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setInt(3, user.getAge());
+
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e);
+        }
+    }
+
+    public boolean contains(User user){
+        try {
+            Class.forName(DRIVER);
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            String query = "select * from " + TABLE_NAME +" where FirstName = ? and LastName = ? and age = ?";
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setInt(3, user.getAge());
+
+            ResultSet rs = ps.executeQuery();
+            boolean res = rs.next();
+            ps.close();
+
+            return res;
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e);
+        }
+        return false;
+    }
+
 }
--- src/main/webapp/loginError.jsp
+++ src/main/webapp/loginError.jsp
@@ -2,15 +2,15 @@
   Created by IntelliJ IDEA.
   User: Levan_Daraselia
   Date: 3/13/2023
-  Time: 5:07 PM
+  Time: 6:08 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Welcome</title>
+    <title>Title</title>
 </head>
 <body>
-<h1>Hello <%=request.getParameter("ragacasaxeli")%> </h1>
+
 </body>
 </html>
