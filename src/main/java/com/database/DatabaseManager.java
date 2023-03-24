c903cd79549e34fa86d4fe782f1708a330e42ad1
Darasa <darasa.daraselia@gmail.com>
2023-03-24 19:17:36 +0400

    Modify DatabaseManager Selectupdate function

--- src/main/java/com/database/DatabaseManager.java
+++ src/main/java/com/database/DatabaseManager.java
@@ -1,7 +1,7 @@
 package com.database;
 
 import java.sql.*;
-
+import java.sql.PreparedStatement;
 public class DatabaseManager {
     public static final String DATABASE_NAME = "baza";
     public static final String URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
@@ -55,29 +55,6 @@ public class DatabaseManager {
     }
 
 
-//    public void selectExample() {
-//        try {
-//            Class.forName(DRIVER);
-//            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//
-//            String query = "select * from " + TABLE_NAME;
-//
-//            Statement st = conn.createStatement();
-//            ResultSet rs = st.executeQuery(query);
-//
-//            while(rs.next()) {
-//                String firstName = rs.getString("firstName");
-//                String lastName = rs.getString("lastName");
-//                String userName = rs.getString("userName");
-//                String passWord = rs.getString("passW");
-//            }
-//            st.close();
-//        } catch (Exception e) {
-//            System.err.println("Got an exception! ");
-//            System.err.println(e);
-//        }
-//    }
-
     public User getUser(String email,String password){
         try {
             Class.forName(DRIVER);
@@ -114,19 +91,21 @@ public class DatabaseManager {
             Class.forName(DRIVER);
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 
-            String query = "update" + TABLE_NAME + "(Name, Surname, UsernameOrEmail, Profession, Passw) values (?, ?, ?, ?, ?)";
+            String query = "UPDATE " + TABLE_NAME + " SET Name=?, Surname=?, UsernameOrEmail=?, Profession=?, Passw=?";
 
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query);
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getUserName());
+            ps.setString(4, user.getProfes());
+            ps.setString(5, user.getPassW());
 
-            while(rs.next()) {
-                String firstName = rs.getString("name");
-                String lastName = rs.getString("surname");
-                String userName = rs.getString("usernameOrEmail");
-                String passWord = rs.getString("passW");
-                String profession = rs.getString("profession");
-            }
-            st.close();
+            int rowsAffected = ps.executeUpdate();
+            System.out.println(rowsAffected + " row(s) affected.");
+
+            ps.close();
+            conn.close();
+
         } catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e);
